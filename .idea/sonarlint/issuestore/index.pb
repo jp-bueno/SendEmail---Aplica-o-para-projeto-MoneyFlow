aa0bc9a799e2dd7ef95e41e0985ca7fef71669c8
joao.pedro <joaopedrobueno2910@gmail.com>
2024-05-14 20:55:02 -0300

    Commit init
    Commit init

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,4 +2,4 @@
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 V
-&src/main/java/br/com/jpbueno/Main.java,e\8\e8963ea505ca2b3a864f7830df9942030b78e89b
+&src/main/java/br/com/jpbueno/Mail.java,d\c\dcd19af5c19a1bbb39de29242f912b25c33b59c1
--- src/main/java/br/com/jpbueno/Mail.java
+++ src/main/java/br/com/jpbueno/Mail.java
@@ -1,7 +1,59 @@
 package br.com.jpbueno;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class Mail {
+    Session newSession = null;
+    MimeMessage mimeMessage = new MimeMessage(newSession);
+    public static void main(String[] args) throws MessagingException {
+
+        Mail mail = new Mail();
+        mail.setupServerProperties();
+        mail.draftEmail();
+        mail.sendEmail();
+    }
+
+    private MimeMessage draftEmail() throws AddressException, MessagingException {
+
+        // Insira o email:
+        String[] emailReceiptients = {"***@gmail.com","***@gmail.com"};
+        String emailSubject = "Fala ai seu cabeça de nós todos";
+        String emailBody = "Esse é o corpo do EMAIL seu chupis";
+
+        for (String emailReceiptient : emailReceiptients) {
+            mimeMessage.addRecipients(Message.RecipientType.TO, emailReceiptient);
+        }
+
+        mimeMessage.setSubject(emailSubject);
+
+        MimeBodyPart bodyPart = new MimeBodyPart();
+        bodyPart.setContent(emailBody, "text/html");
+        MimeMultipart multipart = new MimeMultipart();
+        multipart.addBodyPart(bodyPart);
+        mimeMessage.setContent(multipart);
+        return mimeMessage;
+    }
+
+    private void setupServerProperties() {
+        Properties properties = System.getProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        newSession = Session.getDefaultInstance(properties, null);
+    }
+
+    private void sendEmail() throws MessagingException {
+        // Credenciais SMTP:
+        String fromUser = "***#gmail.com";
+        String fromPassword = "******";
+        String emailHost = "smtp.gmail.com";
+        Transport transport = newSession.getTransport("smtp");
+        transport.connect(emailHost,fromUser, fromPassword);
+        transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+        transport.close();
+        System.out.println("Email successfully sent");
     }
 }
